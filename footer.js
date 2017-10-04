4b475715be838572aade3ed1360ed346ad738e57
gytisve <32506411+gytisve@users.noreply.github.com>
2017-10-04 08:46:20 +0200

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.getElementById("footer").innerHTML = 
+'<footer> 																																				'+
+'			<i class="fa fa-question-circle" aria-hidden="true"></i><a href="omdennesiden.html">Om denne siden</a></span></div><br>							'+
+'			<i class="fa fa-facebook-official" aria-hidden="true"></i> <a href="https://www.facebook.com/gytis.venckus.5/">Gytis Venckus</a><br>		'+
+'			<i class="fa fa-envelope-o" aria-hidden="true"></i><a href="mailto:gytisv@hiof.no">gytisv@hiof.no</a>										'+	
+'</footer>																																				'																																		
+
+
---
+++ navigation.js
@@ -0,0 +1,25 @@
+document.getElementById("navigation").innerHTML = 
+'<nav>																														'+
+'          <ul>																												'+
+'            <li>																											'+
+'             <a href="/~gytisv/index.html">Hjem</a>																					'+
+'            </li>																											'+
+'            <li class="dropdown">																							'+
+'              <a class="dropbutton" href="#" onclick="return false">Innføring i programmering</a>							'+
+'              <div class="dropdown-content">																				'+
+'                <a href="/~gytisv/progr1/oppgaver1.html">Obligatorisk oppgave 1</a> <a href="/~gytisv/progr2/oblig2.html">Obligatorisk oppgave 2</a>	'+
+'               <a href="oppgaver3.html">Obligatorisk oppgave 3</a> <a href="oppgaver4.html">Obligatorisk oppgave 4</a>	'+
+'              </div>																										'+		
+'            </li>																											'+
+'            <li class="dropdown">																							'+
+'              <a class="dropbutton" href="#" onclick="return false">Webutvikling</a>										'+
+'              <div class="dropdown-content">																				'+
+'                <a href="/~gytisv/web1/oppgave1.html">Obligatorisk oppgave 1</a> <a href="oppgave2.html">Obligatorisk oppgave 2</a>		'+
+'                <a href="oppgave3.html">Obligatorisk oppgave 3</a> <a href="oppgave4.html">Obligatorisk oppgave 4</a>		'+
+'              </div>																										'+
+'            </li>																											'+
+'            <li>																											'+
+'              <a href="/~gytisv/makerspace.html">MakerSpace</a>																		'+
+'            </li>																											'+
+'          </ul>																											'+
+' </nav>																													'												
